e39268aab97756ed19241d8bdcb6a0fd690fc893
JinTTTT <tj19970215@gmail.com>
2026-02-07 19:06:29 +0100

    bridge test 7

--- tutorialPlan/YourSampler.cpp
+++ tutorialPlan/YourSampler.cpp
@@ -1,121 +1,22 @@
 #include <chrono>
-#include <cstddef>
-#include <random>
 #include <rl/plan/SimpleModel.h>
 #include "YourSampler.h"
-#include <iostream>
 
 namespace rl
 {
     namespace plan
     {
         YourSampler::YourSampler() :
-            Sampler(),
-            randDistribution(0, 1),
-            randEngine(::std::random_device()()),
+            UniformSampler(),
+            sigma(nullptr),
             gaussDistribution(0, 1),
             gaussEngine(::std::random_device()()),
-            sigma(nullptr),
-            bridgeTestRatio(static_cast<::rl::math::Real>(5) / static_cast<::rl::math::Real>(6))
+            ratio(static_cast< ::rl::math::Real>(5) / static_cast< ::rl::math::Real>(6))
         {
         }
 
         YourSampler::~YourSampler()
         {
-        }
-
-        ::rl::math::Vector
-        YourSampler::generate()
-        {
-            if (this->rand() <= this->bridgeTestRatio)
-            {
-                return this->generateBridgeTest();
-            }
-            else
-            {
-                return this->generateUniform();
-            }
-        }
-
-        ::rl::math::Vector
-        YourSampler::generateUniform()
-        {
-            ::rl::math::Vector sampleq(this->model->getDof());
-            ::rl::math::Vector maximum(this->model->getMaximum());
-            ::rl::math::Vector minimum(this->model->getMinimum());
-
-            for (::std::size_t i = 0; i < this->model->getDof(); ++i)
-            {
-                sampleq(i) = minimum(i) + this->rand() * (maximum(i) - minimum(i));
-            }
-
-            return sampleq;
-        }
-
-        ::rl::math::Vector
-        YourSampler::generateBridgeTest()
-        {
-            while (true)
-            {
-                ::rl::math::Vector q1 = this->generateUniform();
-                
-                this->model->setPosition(q1);
-                this->model->updateFrames();
-                
-                if (this->model->isColliding())
-                {
-                    ::rl::math::Vector q2 = this->generateGaussianNear(q1);
-                    
-                    this->model->setPosition(q2);
-                    this->model->updateFrames();
-                    
-                    if (this->model->isColliding())
-                    {
-                        ::rl::math::Vector midpoint(this->model->getDof());
-                        this->model->interpolate(q1, q2, 0.5, midpoint);
-                        
-                        this->model->setPosition(midpoint);
-                        this->model->updateFrames();
-                        
-                        if (!this->model->isColliding())
-                        {
-                            return midpoint;
-                        }
-                    }
-                }
-            }
-        }
-
-        ::rl::math::Vector
-        YourSampler::generateGaussianNear(const ::rl::math::Vector& center)
-        {
-            ::rl::math::Vector result(this->model->getDof());
-            
-            for (::std::size_t i = 0; i < this->model->getDof(); ++i)
-            {
-                ::rl::math::Real gaussianNoise = this->gauss();
-                
-                if (this->sigma != nullptr)
-                {
-                    result(i) = center(i) + gaussianNoise * (*this->sigma)(i);
-                }
-                else
-                {
-                    ::rl::math::Real range = this->model->getMaximum()(i) - this->model->getMinimum()(i);
-                    ::rl::math::Real defaultSigma = 0.1 * range;
-                    result(i) = center(i) + gaussianNoise * defaultSigma;
-                }
-            }
-            
-            this->model->clip(result);
-            
-            return result;
-        }
-
-        ::std::uniform_real_distribution< ::rl::math::Real>::result_type
-        YourSampler::rand()
-        {
-            return this->randDistribution(this->randEngine);
         }
 
         ::std::normal_distribution< ::rl::math::Real>::result_type
@@ -124,37 +25,54 @@ namespace rl
             return this->gaussDistribution(this->gaussEngine);
         }
 
+        ::rl::math::Vector
+        YourSampler::generateCollisionFree()
+        {
+            if (this->rand() > this->ratio)
+            {
+                return this->generate();
+            }
+            else
+            {
+                ::rl::math::Vector gauss(this->model->getDof());
+                ::rl::math::Vector pos(this->model->getDof());
+                
+                while (true)
+                {
+                    ::rl::math::Vector q1 = this->generate();
+                    this->model->setPosition(q1);
+                    this->model->updateFrames();
+
+                    if (this->model->isColliding())
+                    {
+                        for (::std::size_t i = 0; i < this->model->getDof(); ++i)
+                        {
+                            gauss(i) = this->gauss();
+                        }
+
+                        ::rl::math::Vector q2 = this->model->generatePositionGaussian(gauss, q1, *this->sigma);
+
+                        if (this->model->isColliding())
+                        {
+                            this->model->interpolate(q1, q2, static_cast< ::rl::math::Real>(0.5), pos);
+                            this->model->setPosition(pos);
+                            this->model->updateFrames();
+                
+                            if (!this->model->isColliding())
+                            {
+                                return pos;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         void
         YourSampler::seed(const ::std::mt19937::result_type& value)
         {
-            this->randEngine.seed(value);
             this->gaussEngine.seed(value);
+            this->randEngine.seed(value);
         }
-
-        void
-        YourSampler::setSigma(::rl::math::Vector* sigma)
-        {
-            this->sigma = sigma;
-        }
-
-        ::rl::math::Vector*
-        YourSampler::getSigma() const
-        {
-            return this->sigma;
-        }
-
-        void
-        YourSampler::setBridgeTestRatio(const ::rl::math::Real& ratio)
-        {
-            this->bridgeTestRatio = ratio;
-        }
-
-        ::rl::math::Real
-        YourSampler::getBridgeTestRatio() const
-        {
-            return this->bridgeTestRatio;
-        }
-
     }
 }
-
--- tutorialPlan/YourSampler.h
+++ tutorialPlan/YourSampler.h
@@ -1,44 +1,30 @@
 #ifndef _YOURSAMPLER_H_
 #define _YOURSAMPLER_H_
 
-#include <rl/plan/Sampler.h>
+#include <rl/plan/UniformSampler.h>
 #include <random>
 
 namespace rl
 {
     namespace plan
     {
-        class YourSampler : public Sampler
+        class YourSampler : public UniformSampler
         {
         public:
             YourSampler();
             virtual ~YourSampler();
 
-            ::rl::math::Vector generate();
+            ::rl::math::Vector generateCollisionFree();
             virtual void seed(const ::std::mt19937::result_type& value);
 
-            void setSigma(::rl::math::Vector* sigma);
-            ::rl::math::Vector* getSigma() const;
-
-            void setBridgeTestRatio(const ::rl::math::Real& ratio);
-            ::rl::math::Real getBridgeTestRatio() const;
+            ::rl::math::Vector* sigma;
+            ::rl::math::Real ratio;
 
         protected:
-            ::std::uniform_real_distribution< ::rl::math::Real>::result_type rand();
             ::std::normal_distribution< ::rl::math::Real>::result_type gauss();
-
-            ::rl::math::Vector generateUniform();
-            ::rl::math::Vector generateBridgeTest();
-            ::rl::math::Vector generateGaussianNear(const ::rl::math::Vector& center);
-
-            ::std::uniform_real_distribution< ::rl::math::Real> randDistribution;
-            ::std::mt19937 randEngine;
 
             ::std::normal_distribution< ::rl::math::Real> gaussDistribution;
             ::std::mt19937 gaussEngine;
-
-            ::rl::math::Vector* sigma;
-            ::rl::math::Real bridgeTestRatio;
 
         private:
         };
